119cb1383470a28b7651903b1b9de8af7f8e73a5
Guan Wei <phuaguanwei99@gmail.com>
2023-02-03 14:12:03 +0800

    Thousands seperator

--- src/components/UI/EmployeeCard/CardContent.tsx
+++ src/components/UI/EmployeeCard/CardContent.tsx
@@ -6,12 +6,17 @@ type ICardContent = {
 };
 
 const CardContent: React.FC<ICardContent> = ({ employee }) => {
+  // Regex to add commas as thousands seperators.
+  const showSalary = (salary: number): string => {
+    return "$" + salary.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
   return (
     <div className="content-display">
       <ul className="list-style">
         <li className="employee__name">{employee.name}</li>
         <li className="employee__details">{employee.department}</li>
-        <li className="employee__details">{employee.salary}</li>
+        <li className="employee__details">{showSalary(employee.salary)}</li>
       </ul>
     </div>
   );
